a097d37a1b15d73e0425d8febdda1b2c209f2876
Andrey Madiev <madiev@lanbilling.ru>
2015-08-19 18:00:52 +0300

    fixed wrong url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='lanbilling',
       version='0.0.1',
       description='The API wrapper for LANBilling',
-      url='http://github.com/storborg/funniest',
+      url='https://git.lanbilling.ru/madiev/lanbilling',
       author='Andrey Madiev',
       author_email='madiev@lanbilling.ru',
       license='GPL2',
